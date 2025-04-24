7496816faf67b4c4c88b84d6e013c6c0ec18ee49
Landon94 <lkarkut@uci.edu>
2025-04-24 21:46:57 +0000

    Outline for stats.

--- stats.py
+++ stats.py
@@ -1,4 +1,16 @@
+from bs4 import BeautifulSoup
+
 UNIQUE_PAGES = 0
 LONGEST_PAGE = 0
 COMMON_WORDS = {}
 SUBDOMAINS = {}
+
+def calculate_stats(resp):
+    pass
+
+def calculate_num_words():
+    pass
+
+def common_words():
+    pass
+

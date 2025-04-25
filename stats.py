3c8a8aed96ef6d0d7d151962b3810f7c1797a67e
Landon94 <lkarkut@uci.edu>
2025-04-25 05:26:31 +0000

    Added comments, Implemented write_stats, fixed minor variable naming issues.

--- stats.py
+++ stats.py
@@ -4,7 +4,7 @@ from urllib.parse import urlparse
 UNIQUE_PAGES = 0
 LONGEST_PAGE = 0
 LONGEST_PAGE_COUNT = 0
-PAGES = {}
+PAGES = set()
 COMMON_WORDS = {}
 SUBDOMAINS = {}
 #Note words with ' will not be filtered out because of tokenizer treating it as seperator
@@ -30,29 +30,43 @@ ENGLISH_STOP_WORDS = stopwords = {
     "you'll", "you're", "you've", "your", "yours", "yourself", "yourselves"
 }
 
+
+'''
+Notes: call calculate_stats in extract_next_links().
+Can either add write_stats() to calculate_stats()
+or call it seperately in extract_next_links().
+Can also add loader function to detect if stats.txt
+exist and load values from there into global variables
+'''
+#Main function for calculating all stats
 def calculate_stats(resp):
 
     soup = BeautifulSoup(resp.raw_response.content, 'html.parser')
 
     parsed_url = urlparse(resp.raw_response.url)
-    subdomain = parsed_url.hostname.split('.')[0]
-    parsed_url._replace(fragment="").geturl()
-    unique_pages(parsed_url)
+    url = parsed_url._replace(fragment="").geturl()
     
-    '''
+    subdomain = parsed_url.hostname.split('.')[0]
+    calcuate_subdomains(subdomain)
+    
+    unique_pages(url)
+    
     text = soup.get_text()
     tokens = tokenizer(text)
-    calculate_num_words(tokens)
+    calculate_num_words(url, tokens)
+    computeWordFrequencies(tokens)
 
-    '''
 
-
+#Computes the number of unique pages
 def unique_pages(url) -> None:
+    global UNIQUE_PAGES
     PAGES.add(url)
     UNIQUE_PAGES = len(PAGES)
 
 
+#Computes the word count of the page and updates global stats variables
 def calculate_num_words(url, tokens) -> None:
+    global LONGEST_PAGE_COUNT, LONGEST_PAGE
     word_count = len(tokens)
 
     if word_count > LONGEST_PAGE_COUNT:
@@ -60,21 +74,24 @@ def calculate_num_words(url, tokens) -> None:
         LONGEST_PAGE = url
 
 
-def calcuate_subdomains(subdomain):
+#Adds instance of subdomain to dictionary or increases frequency of instance
+def calcuate_subdomains(subdomain) -> None:
     SUBDOMAINS[subdomain] = SUBDOMAINS.get(subdomain, 0) + 1
 
 
+#Adds instance of word to dictionary or increases frequency of word
 def computeWordFrequencies(TokenList) -> None:
     for word in TokenList:
         if not word in ENGLISH_STOP_WORDS:
             COMMON_WORDS[word] = COMMON_WORDS.get(word, 0) + 1
 
 
+#Tokenizes Text
 def tokenize(text) -> list:
     tokens = []
     word = ""
     word_chars = []
-        for char in line:
+        for char in text:
             if (char.isascii() and char.isalnum()):
                 word_chars.append(char)
             else:
@@ -87,5 +104,20 @@ def tokenize(text) -> list:
     return tokens
 
 
-def write_stats():
-    pass
+#Write down global stats to txt file
+def write_stats() -> None:
+    with open('stats.txt', 'w') as file:
+        file.write(f"Unique Pages: {UNIQUE_PAGES}\n")
+        file.write(f"Longest Pages Word Count: {LONGEST_PAGE_COUNT}\n")
+
+        #Sorts word by frequency
+        sorted_words = sorted(COMMON_WORDS.items(), key = lambda freq:freq[1], reverse = True)
+        file.write(f"50 most common words\n")
+        for word, freq in sorted_words[:50]:
+            file.write(f"{word} - {count}\n")
+
+        #Sorts subdomain alphabetically
+        sorted_sub = sorted(SUBDOMAINS.items(), key = lambda freq:freq[0])
+        file.write(f"Subdomains\n")
+        for sub, freq in sorted_sub:
+            file.write(f"{sub} - {freq}\n")
